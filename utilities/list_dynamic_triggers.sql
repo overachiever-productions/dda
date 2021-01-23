ce963418ed3a93b63ba5b651d17a90187b1150aa
Michael K. Campbell <mike@overachiever.net>
2021-01-23 15:32:21 -0800

    Mods to exclude TEMPLATE from listed triggers.
    i.e., don't want to list the template - it's ... special/weird/holy/blameless.

--- utilities/list_dynamic_triggers.sql
+++ utilities/list_dynamic_triggers.sql
@@ -7,7 +7,7 @@ AS
 	SET NOCOUNT ON; 
 
 	-- {copyright}
-
+	
 	SELECT 
 		(SELECT QUOTENAME(SCHEMA_NAME(o.[schema_id])) + N'.' + QUOTENAME(OBJECT_NAME(o.[object_id])) FROM sys.objects o WHERE o.[object_id] = t.[parent_id]) [parent_table],
 		(SELECT QUOTENAME(SCHEMA_NAME(o.[schema_id])) + N'.' + QUOTENAME(t.[name]) FROM sys.objects o WHERE o.[object_id] = t.[object_id]) [trigger_name],
@@ -23,7 +23,10 @@ AS
 		sys.triggers t
 		INNER JOIN sys.[extended_properties] p ON t.[object_id] = p.[major_id]
 	WHERE 
-		p.[name] = N'DDATrigger' AND p.[value] = 'true';
+		p.[name] = N'DDATrigger' AND p.[value] = 'true'
+		AND 
+			-- don't show the dynamic trigger TEMPLATE - that'll just cause confusion:
+			t.[parent_id] <> (SELECT [object_id] FROM sys.objects WHERE [schema_id] = SCHEMA_ID('dda') AND [name] = N'trigger_host');
 
 	RETURN 0;
 GO
