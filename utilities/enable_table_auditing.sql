5b4cf16c9c9c2059ff1888e7f664c03748c81d84
Michael K. Campbell <mike@overachiever.net>
2021-02-05 16:41:09 -0800

    DDA-16: Minor text/output changes to error details

--- utilities/enable_table_auditing.sql
+++ utilities/enable_table_auditing.sql
@@ -39,7 +39,7 @@ AS
 		FROM sys.[triggers] t INNER JOIN sys.[extended_properties] p ON t.[object_id] = p.[major_id] 
 		WHERE t.[parent_id] = @objectID AND p.[name] = N'DDATrigger' AND p.[value] = 'true'
 	) BEGIN
-		RAISERROR(N'Target Table (%s) already has a dynamic data auditing (DDA) trigger defined. Please review existing triggers on table.', 16, 1, @objectName);
+		RAISERROR(N'Target Table %s already has a dynamic data auditing (DDA) trigger defined. Please review existing triggers on table.', 16, 1, @objectName);
 		RETURN -20;
 	END;
 
