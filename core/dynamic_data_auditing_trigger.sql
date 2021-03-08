b4af2b96644f0acfc003322400a7c7b828355d01
Michael K. Campbell <mike@overachiever.net>
2021-03-08 14:45:27 -0800

    DDA-30: Enable Maintenance Bypass for DDA Triggers
    Randomized CONTEXT_INFO() 'key' will be deployed into each db/deployment.

--- core/dynamic_data_auditing_trigger.sql
+++ core/dynamic_data_auditing_trigger.sql
@@ -57,6 +57,12 @@ AS
 
 	-- {copyright}
 
+	DECLARE @context varbinary(128) = ISNULL(CONTEXT_INFO(), 0x0);
+	IF @context = 0x999090000000000000009999 BEGIN -- set to a random/unique value at deployment
+		PRINT 'Dynamic Data Auditing Trigger bypassed.';
+		GOTO Cleanup; 
+	END;
+
 	DECLARE @tableName sysname, @schemaName sysname;
 	SELECT 
 		@schemaName = SCHEMA_NAME([schema_id]),
